85eb034073d88088dabefd3b890f056e04d67090
Regina Gaigl <regina.gaigl@gmail.com>
2024-12-16 15:46:12 +0100

    font and images

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -22,7 +22,11 @@ class DrawableObject {
     }
 
     draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        try {
+            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        } catch (error) {   
+            console.log('Error while drawing image', error, this.img.src);
+        }
     }
 
     drawRect(ctx) {
